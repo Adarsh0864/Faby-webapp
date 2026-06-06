1df22f7a6e2821072cfd6dd81b8420ee992efa02
Adarsh Mishra <112790875+Adarsh0864@users.noreply.github.com>
2026-06-07 01:05:09 +0530

    Increase assetsInlineLimit to 500KB to ensure images are inlined

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 512000, // 500KB — inline all images into JS bundle
+  },
 })
